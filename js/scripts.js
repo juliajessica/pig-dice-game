186823da6597e358a07dfd022ffffab8d436a175
julia sheremet and matt miller <matt@>
2018-04-04 10:28:28 -0700

    add function to achieve second spec.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,29 @@
 //Business Logic
+// var p1RunningTotal = 0;
+var p1RollArray =[];
+
 function rollDice(number) {
   return Math.floor(Math.random() * Math.floor(number)) + 1;
 }
+
+function getSum(total, number) {
+  return total + number;
+}
+
+function runningRollDice(number) {
+  p1RollArray.push(rollDice(6))
+  return p1RollArray.reduce(getSum);
+}
+
+
 
 
 //User Logic
 $(function(){
   $("#roll-button").click(function(event){
-    $("#p1runningTotal").text(rollDice(6));
+    var p1CurrentRunningTotal = runningRollDice(6);
+    $("#p1runningTotal").text(p1CurrentRunningTotal);
+    console.log(p1RollArray[p1RollArray.length-1]);
     $("#player1-scorecard").show();
     event.preventDefault();
   });
