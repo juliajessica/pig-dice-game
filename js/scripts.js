a3b64e5b3f296f3745744bd83f98834c2e240fe3
julia sheremet and matt miller <julia@>
2018-04-04 11:34:27 -0700

    refactor js to run spec 4 properly.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
 //Business Logic
-// var p1RunningTotal = 0;
-var rollArray =[];
+function Player(userName,score) {
+  this.userName = userName;
+  this.score = score;
+}
+
+Player.prototype.totalScore = function (number) {
+  return this.score += number;
+};
+
+var playerArray = [];
+var rollArray = [0];
 
 function rollDice(number) {
   return Math.floor(Math.random() * Math.floor(number)) + 1;
@@ -10,40 +19,41 @@ function getSum(total, number) {
   return total + number;
 }
 
+function rollArrayTotal(sum) {
+  return rollArray.reduce(sum)
+}
+
 function runningRollDice(number) {
   rollArray.push(rollDice(6))
-  return rollArray.reduce(getSum);
+  return rollArrayTotal(getSum);
 }
 
 function emptyRollArray() {
   return rollArray.length = 0
 }
 
-
-
 //User Logic
 $(function(){
   $("#player1-form").submit(function(event){
     event.preventDefault();
-
-  var p1Name = $("#p1InputName").val();
-  console.log(p1Name);
-  $("#player1-name").text(p1Name);
-
-  $("#player1-scorecard").fadeIn();
-
-
+    var p1Name = $("#p1InputName").val();
+    var playerObject = new Player(p1Name, 0);
+    playerArray.push(playerObject);
+    $("#player1-name").text(p1Name);
+    $("#player1-scorecard").fadeIn();
   });
+
   $("#roll-button").click(function(){
     var p1CurrentRunningTotal = runningRollDice(6);
     $("#p1runningTotal").text(p1CurrentRunningTotal);
     // console.log(p1RollArray[p1RollArray.length-1]);
     $("#player1-scorecard").show();
   });
+
   $("#hold-button").click(function(){
-    $("#p1scoreTotal").text(rollArray.reduce(getSum));
+    playerArray[0].totalScore(rollArrayTotal(getSum));
+    $("#p1scoreTotal").text(playerArray[0].score);
     emptyRollArray();
     $("#p1runningTotal").text(0);
-    console.log(rollArray);
   });
 });
