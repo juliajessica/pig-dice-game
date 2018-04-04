edb28ccd55361587d043f9e99b672e9f67155b74
julia sheremet and matt miller <julia@>
2018-04-04 10:51:07 -0700

    Add click listener for hold and function to achieve third and fourth specs.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 //Business Logic
 // var p1RunningTotal = 0;
-var p1RollArray =[];
+var rollArray =[];
 
 function rollDice(number) {
   return Math.floor(Math.random() * Math.floor(number)) + 1;
@@ -11,20 +11,39 @@ function getSum(total, number) {
 }
 
 function runningRollDice(number) {
-  p1RollArray.push(rollDice(6))
-  return p1RollArray.reduce(getSum);
+  rollArray.push(rollDice(6))
+  return rollArray.reduce(getSum);
 }
 
+function emptyRollArray() {
+  return rollArray.length = 0
+}
 
 
 
 //User Logic
 $(function(){
-  $("#roll-button").click(function(event){
+  $("#player1-form").submit(function(event){
+    event.preventDefault();
+
+  var p1Name = $("#p1InputName").val();
+  console.log(p1Name);
+  $("#player1-name").text(p1Name);
+
+  $("#player1-scorecard").fadeIn();
+
+
+  });
+  $("#roll-button").click(function(){
     var p1CurrentRunningTotal = runningRollDice(6);
     $("#p1runningTotal").text(p1CurrentRunningTotal);
-    console.log(p1RollArray[p1RollArray.length-1]);
+    // console.log(p1RollArray[p1RollArray.length-1]);
     $("#player1-scorecard").show();
-    event.preventDefault();
+  });
+  $("#hold-button").click(function(){
+    $("#p1scoreTotal").text(rollArray.reduce(getSum));
+    emptyRollArray();
+    $("#p1runningTotal").text(0);
+    console.log(rollArray);
   });
 });
